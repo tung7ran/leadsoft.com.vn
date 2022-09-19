1ece0385f828e50e3fd1b4f0db5ee79ac520b86b
kieuvanh1999 <01248991825lala@gmail.com>
2022-09-19 15:26:07 +0700

    delete comment and space

--- js/about-us.js
+++ js/about-us.js
@@ -15,3 +15,26 @@ wow = new WOW(
             }
         );
 wow.init();
+
+$('.counter').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+  
+    {
+  
+      duration: 20000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+        //alert('finished');
+      }
+  
+    });  
+  });
