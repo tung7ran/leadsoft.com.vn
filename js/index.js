e6263e8499163e747627d5c31db64bc8c68a84f3
kieuvanh1999 <01248991825lala@gmail.com>
2022-09-22 17:12:20 +0700

    upload - update
    thêm partical js
    slick js

--- js/index.js
+++ js/index.js
@@ -22,9 +22,7 @@ $('.counter').each(function() {
     $({ countNum: $this.text()}).animate({
       countNum: countTo
     },
-  
     {
-  
       duration: 1000,
       easing:'linear',
       step: function() {
@@ -32,41 +30,70 @@ $('.counter').each(function() {
       },
       complete: function() {
         $this.text(this.countNum);
-        //alert('finished');
       }
   
     });  
   });
 
-gsap.timeline({
-    scrollTrigger: {
-    trigger: ".about-us-content-box-first",
-    start: "center center",
-    end: "bottom top",
-    markers: false,
-    scrub: true,
-    pin: true,
-    }
-})
-.from(".about-us-content-box-first-textFirst", {x: innerWidth * 2})
-.from(".about-us-content-box-first-textSecond", {x: innerWidth * -1})
-.from(".about-us-content-box-first-textThird", {x: innerWidth * 1}) 
-.from(".about-us-content-box-first-img", {y: innerWidth * 1})
+  $('.counters').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 2000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+  
+    });  
+  });
 
-gsap.timeline({
-  scrollTrigger: {
-  trigger: ".about-us-content-box-second",
-  start: "center center",
-  end: "bottom top",
-  markers: false,
-  scrub: true,
-  pin: true,
-  }
-})
-.from(".about-us-content-box-second-textFourth", {x: innerWidth * 1})
-.from(".about-us-content-box-second-textFifth", {x: innerWidth * -1})
-.from(".about-us-content-box-second-textSixth", {x: innerWidth * 1}) 
-.from(".about-us-content-box-second-img", {y: innerWidth * 1})
+  $('.content-news-our-team-container').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    pauseOnFocus: true,
+    Swipe: true,
+    easing: "linner",
+    
+  });
+// gsap.timeline({
+//     scrollTrigger: {
+//     trigger: ".about-us-content-box-first",
+//     start: "center center",
+//     end: "bottom top",
+//     markers: false,
+//     scrub: true,
+//     pin: true,
+//     }
+// })
+// .from(".about-us-content-box-first-textFirst", {x: innerWidth * 2})
+// .from(".about-us-content-box-first-textSecond", {x: innerWidth * -1})
+// .from(".about-us-content-box-first-textThird", {x: innerWidth * 2}) 
+// .from(".about-us-content-box-first-img", {y: innerWidth * 1})
+
+// gsap.timeline({
+//   scrollTrigger: {
+//   trigger: ".about-us-content-box-second",
+//   start: "center center",
+//   end: "bottom top",
+//   markers: false,
+//   scrub: true,
+//   pin: true,
+//   }
+// })
+// .from(".about-us-content-box-second-textFourth", {x: innerWidth * 1})
+// .from(".about-us-content-box-second-textFifth", {x: innerWidth * -1})
+// .from(".about-us-content-box-second-textSixth", {x: innerWidth * 1}) 
 
 var btn = $('#button');
 
@@ -82,3 +109,4 @@ btn.on('click', function(e) {
   e.preventDefault();
   $('html, body').animate({scrollTop:0}, '300');
 });
+
