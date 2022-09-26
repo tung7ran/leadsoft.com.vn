4da07e42412e31ca7cd7dd8418568ec53c4bc23f
kieuvanh1999 <01248991825lala@gmail.com>
2022-09-26 17:29:50 +0700

    upload js scss
    thêm responsive tất cả các trang

--- js/index.js
+++ js/index.js
@@ -1,4 +1,4 @@
-VanillaTilt.init(document.querySelectorAll(".content-news-our-team-card"), {
+VanillaTilt.init(document.querySelectorAll(".content-home-our-team-card"), {
   max: 25,
   speed: 400,
   glare: true,
@@ -22,9 +22,7 @@ $('.counter').each(function() {
     $({ countNum: $this.text()}).animate({
       countNum: countTo
     },
-  
     {
-  
       duration: 1000,
       easing:'linear',
       step: function() {
@@ -32,53 +30,193 @@ $('.counter').each(function() {
       },
       complete: function() {
         $this.text(this.countNum);
-        //alert('finished');
       }
   
     });  
   });
 
-gsap.timeline({
-    scrollTrigger: {
-    trigger: ".about-us-content-box-first",
-    start: "center center",
-    end: "bottom top",
-    markers: false,
-    scrub: true,
-    pin: true,
-    }
-})
-.from(".about-us-content-box-first-textFirst", {x: innerWidth * 2})
-.from(".about-us-content-box-first-textSecond", {x: innerWidth * -1})
-.from(".about-us-content-box-first-textThird", {x: innerWidth * 1}) 
-.from(".about-us-content-box-first-img", {y: innerWidth * 1})
+  $('.counters').each(function() {
+    var $this = $(this),
+        countTo = $this.attr('data-count');
+    
+    $({ countNum: $this.text()}).animate({
+      countNum: countTo
+    },
+    {
+      duration: 2000,
+      easing:'linear',
+      step: function() {
+        $this.text(Math.floor(this.countNum));
+      },
+      complete: function() {
+        $this.text(this.countNum);
+      }
+  
+    });  
+  });
 
-gsap.timeline({
-  scrollTrigger: {
-  trigger: ".about-us-content-box-second",
-  start: "center center",
-  end: "bottom top",
-  markers: false,
-  scrub: true,
-  pin: true,
-  }
-})
-.from(".about-us-content-box-second-textFourth", {x: innerWidth * 1})
-.from(".about-us-content-box-second-textFifth", {x: innerWidth * -1})
-.from(".about-us-content-box-second-textSixth", {x: innerWidth * 1}) 
-.from(".about-us-content-box-second-img", {y: innerWidth * 1})
+  $('.content-home-our-team-container').slick({
+    slidesToShow: 4,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    arrows: false,
+    pauseOnFocus: true,
+    Swipe: true,
+    easing: "linner",
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 3,
+          autoplaySpeed: 1000,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 1,
+          autoplaySpeed: 1000,
+        }
+      }
+    ]
+  });
 
-var btn = $('#button');
+  $('.pc-slider-top').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+});
+
+$('.pc-slider-between').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+});
+
+$('.pc-slider-bottom').slick({
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    dots: false,
+    centerMode: true,
+    focusOnSelect: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    nextArrow: false,
+    prevArrow: false,
+    responsive: [
+      {
+        breakpoint: 769,
+        settings: {
+          slidesToShow: 2,
+        }
+      },
+      {
+        breakpoint: 481,
+        settings: {
+          slidesToShow: 1,
+        }
+      }
+    ]
+});
+// gsap.timeline({
+//     scrollTrigger: {
+//     trigger: ".about-us-content-box-first",
+//     start: "center center",
+//     end: "bottom top",
+//     markers: false,
+//     scrub: true,
+//     pin: true,
+//     }
+// })
+// .from(".about-us-content-box-first-textFirst", {x: innerWidth * 2})
+// .from(".about-us-content-box-first-textSecond", {x: innerWidth * -1})
+// .from(".about-us-content-box-first-textThird", {x: innerWidth * 2}) 
+// .from(".about-us-content-box-first-img", {y: innerWidth * 1})
+
+// gsap.timeline({
+//   scrollTrigger: {
+//   trigger: ".about-us-content-box-second",
+//   start: "center center",
+//   end: "bottom top",
+//   markers: false,
+//   scrub: true,
+//   pin: true,
+//   }
+// })
+// .from(".about-us-content-box-second-textFourth", {x: innerWidth * 1})
+// .from(".about-us-content-box-second-textFifth", {x: innerWidth * -1})
+// .from(".about-us-content-box-second-textSixth", {x: innerWidth * 1}) 
+
+var scrollToTop = $('#scrollToTop');
 
 $(window).scroll(function() {
-  if ($(window).scrollTop() > 300) {
-    btn.addClass('show');
+  if ($(window).scrollTop() > 200) {
+    scrollToTop.addClass('show');
   } else {
-    btn.removeClass('show');
+    scrollToTop.removeClass('show');
   }
 });
 
-btn.on('click', function(e) {
+scrollToTop.on('click', function(e) {
   e.preventDefault();
-  $('html, body').animate({scrollTop:0}, '300');
+  $('html, body').animate({scrollTop:0}, '200');
 });
+
+
+function toggleMenu () {  
+  const navbar = document.querySelector('.header-outside-nav');
+  const burger = document.querySelector('.header-outside-nav-button');
+
+  burger.addEventListener('click', (e) => {    
+    navbar.classList.toggle('show-nav');
+  });    
+  const navbarLinks = document.querySelectorAll('.header-outside-nav a');
+  navbarLinks.forEach(link => {
+    link.addEventListener('click', (e) => {    
+      navbar.classList.toggle('show-nav');
+    }); 
+  })
+   
+}
+toggleMenu();
